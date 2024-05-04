43f1daa5f60c2b27f21617b2a247afecc5a82c08
Dan <danlewismuriuki@gmail.com>
2024-05-04 23:12:51 +0300

    Updated the sed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
-
-# Install Nginx if it not already installed
+# sets up your web servers for the deployment of web_static
 apt-get -y update
 apt-get -y install nginx
-
-# Create the folder /data/ if it doesn’t already exist
-# Create the folder /data/web_static/ if it doesn’t already exist
-# Create the folder /data/web_static/releases/ if it doesn’t already exist
-# Create the folder /data/web_static/shared/ if it doesn’t already exist
-# Create the folder /data/web_static/releases/test/ if it doesn’t already exist
 ufw allow 'Nginx HTTP'
 mkdir -p /data/web_static/
 mkdir -p /data/web_static/releases/test/
@@ -20,15 +13,8 @@ echo "<html>
     Holberton School
   </body>
 </html>" > /data/web_static/releases/test/index.html
-
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-
+ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data
-
-config_file="/etc/nginx/sites-available/default"
-
-sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' $config_file
-
+sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 service nginx restart
-
 exit 0
