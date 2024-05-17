0fa9698b53afb3b3bd28e81f4aa0645f747aeffc
root <root@DL>
2024-05-17 15:51:10 +0300

    feat: Implement Flask web application with specified routes
    This commit implements a Flask web application that listens on 0.0.0.0, port 5000. The application includes the following routes:
    
    /: Displays "Hello HBNB!"
    /hbnb: Displays "HBNB"
    /c/<text>: Displays "C ", followed by the value of the text variable (with underscores replaced by spaces)
    /python/<text>: Displays "Python ", followed by the value of the text variable (with underscores replaced by spaces), with a default value of "is cool"
    /number/<n>: Displays "n is a number" only if n is an integer
    /number_template/<n>: Displays an HTML page with the content "Number: n" inside an H1 tag, only if n is an integer
    The route definitions use the option strict_slashes=False.

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Flask Web Application"""
+
+from flask import render_template
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def Hello_HBNB():
+    """Route to display 'Hello HBNB!'."""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """Route to display 'HBNB'."""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """Route to display 'C' followed by the value of the text variable."""
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is cool'):
+    """Route to display 'Python' followed by the value of the
+        text variable (default is 'is cool').
+    """
+    text = text.replace('_', ' ')
+    return 'python {}'.format(text)
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_text(n):
+    """Display 'n is a number' only if n is an integer."""
+    return '{} is a number'.format(n)
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """Display a HTML page with the content 'Number: n' inside an H1 tag."""
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
