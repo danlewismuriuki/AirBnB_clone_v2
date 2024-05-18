d7ab024f6f83ce7e8a7a306cd4c8a8844b2c9d72
Dan <danlewismuriuki@gmail.com>
2024-05-18 17:59:32 +0300

    feat: Add shebang line to script
    Added a shebang line (`#!/usr/bin/python3`) to the Python script for specifying the interpreter.
    
    This change ensures that the script runs with Python 3 by default.

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,5 @@
-from flask import Flask
 """Flask web applicaton"""
+from flask import Flask
 app = Flask(__name__)
 
 
@@ -8,6 +8,6 @@ def hello_world():
     """Route to display 'Hello HBNB!'."""
     return "Hello HBNB!"
 
-
+# Run the Flask application on host 0.0.0.0 and port 5000
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
