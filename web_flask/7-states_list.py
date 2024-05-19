cf6e3732ef0e9f3d742d747d59ed4d2c3328f8cb
root <root@DL>
2024-05-19 16:54:21 +0300

    feat: Add Flask route to display list of states
    In this commit, a new Flask route '/states_list' is added to the web application. This route fetches all states from the storage engine (DBStorage) and renders them in an HTML page using a template. The states are sorted alphabetically by name before being displayed.

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,75 +1,26 @@
 #!/usr/bin/python3
-"""Flask Web Application"""
+"""Start a Flask web application"""
 
-from flask import render_template
-from flask import Flask
-from models.state import State
+from flask import Flask, render_template
 from models import storage
+from models.state import State
+
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def Hello_HBNB():
-    """Route to display 'Hello HBNB!'."""
-    return 'Hello HBNB!'
-
-
-@app.route('/hbnb', strict_slashes=False)
-def HBNB():
-    """Route to display 'HBNB'."""
-    return 'HBNB'
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    """Route to display 'C' followed by the value of the text variable."""
-    text = text.replace('_', ' ')
-    return 'C {}'.format(text)
-
-
-@app.route('/python/', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_text(text='is cool'):
-    """Route to display 'Python' followed by the value of the
-        text variable (default is 'is cool').
-    """
-    text = text.replace('_', ' ')
-    return 'python {}'.format(text)
-
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def number_text(n):
-    """Display 'n is a number' only if n is an integer."""
-    return '{} is a number'.format(n)
-
-
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    """Display a HTML page with the content 'Number: n' inside an H1 tag."""
-    return render_template('5-number.html', n=n)
-
-
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def number_odd_or_even(n):
-    """Display a HTML page with the content 'Number: n' inside an H1 tag.
-        Number n is 'even|odd' inside the tage BODY
-    """
-    return render_template('6-number_odd_or_even.html', n=n)
-
-
 @app.route('/states_list', strict_slashes=False)
-def list_state():
+def states_list():
     """Displays a HTML page with a list of all states"""
     states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def tear_down(exception):
-    """ Closes sqlAlchemy session """
+def teardown(self):
+    """removes the current SQLAlchemy Session"""
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
